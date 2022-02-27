11faee7fe3d52d5550a1db7b8433f15238e33bb2
Muhammad Abdullah Khan <abdullahkhan.smiu@gmail.com>
2022-02-28 01:53:14 +0500

    Commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:animated_drawers/drawer_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.black,
+  ));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SidebarPage(),
+    );
+  }
+}
